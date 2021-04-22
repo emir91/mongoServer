7c494d8a8ab9b402d37b82308d48dbe5d2c733a4
emir91 <vranacemir@gmail.com>
2021-04-22 13:20:54 +0200

    Create CRUD routes for students.controller and install lodash

--- src/controllers/students.controller.js
+++ src/controllers/students.controller.js
@@ -1,13 +1,71 @@
+import _ from 'lodash';
 import Student from '../models/student.model';
+
+const create = (req, res) => {
+    const student = Student(req.body);
+    student.save((err, data) => {
+        if(err) {
+            return res.status(400).json(err.message);
+        } else {
+            res.status(201).json(data);
+        }
+    });
+}
 
 const list = (req, res) => {
     Student.find((err, data) => {
         if(err){
-            console.log(err);
+            return res.status(400).json(err.message);
         } else {
             res.status(200).json(data);
         }
     });
 }
 
-export default { list }
+const read = (req, res) => {
+    const id = req.params.id;
+    Student.findById(id).exec((err, data) => {
+        if(err){
+            return res.status(400).json(err.message);
+        } else {
+            res.status(200).json(data);
+        }
+    });
+}
+
+const update = (req, res) => {
+    const id = req.params.id;
+    Student.findById(id).exec((err, data) => {
+        if(err || !data){
+            return res.status(400).json('Student not found');
+        } else {
+            const student = _.extend(data, req.body);
+             student.save((err, data) => {
+                if(err) {
+                    return res.status(400).json(err.message);
+                } else {
+                    res.status(200).json(data);
+                }
+            });
+        }
+    });
+}
+
+const remove = (req, res) => {
+    const id = req.params.id;
+    Student.findById(id).exec((err,data) => {
+        if(err || !data) {
+            return res.status(400).json('Student not found');
+        } else {
+            data.remove((err, data) => {
+                if(err) {
+                    return res.status(400).json('Student not found');
+                } else {
+                    res.status(200).json('Student deleted.');
+                }
+            });
+        }
+    });
+}
+
+export default { create, list, read, update, remove }
