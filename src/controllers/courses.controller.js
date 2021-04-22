7d688963b29f0bc059bdbdaffc3824719dcbaae5
emir91 <vranacemir@gmail.com>
2021-04-22 16:41:16 +0200

    Create CRUD for courses controller

--- src/controllers/courses.controller.js
+++ src/controllers/courses.controller.js
@@ -1,13 +1,71 @@
+import _ from 'lodash';
 import Course from '../models/course.model';
+
+const create = (req, res) => {
+    const course = Course(req.body);
+    course.save((err, data) => {
+        if(err) {
+            return res.status(400).json(err.message);
+        } else {
+            res.status(201).json(data);
+        }
+    });
+}
 
 const list = (req, res) => {
     Course.find((err, data) => {
         if(err) {
-            console.log(err);
+            return res.status(400).json(err.message);
         } else {
             res.status(200).json(data);
         }
     });
 }
 
-export default { list };
+const read = (req, res) => {
+    const id = req.params.id;
+    Course.findById(id).exec((err, data) => {
+        if(err) {
+            return res.status(400).json(err.message);
+        } else {
+            res.status(200).json(data);
+        }
+    });
+}
+
+const update = (req, res) => {
+    const id = req.params.id;
+    Course.findBy(id).exec((err, data) => {
+        if(err || !data) {
+            return res.status(400).json('Course not found');
+        } else {
+            const course = _.extend(data, req.body);
+            course.save((err, data) => {
+                if(err) {
+                    return res.status(400).json(err.message);
+                } else {
+                    res.status(200).json(data);
+                }
+            });
+        }
+    }); 
+}
+
+const remove = (req, res) => {
+    const id = req.params.id;
+    Course.findById(id).exec((err, data) => {
+         if(err || !data) {
+            return res.status(400).json('Course not found');
+        } else {
+            data.remove((err, data) => {
+                if(err) {
+                    return res.status(400).json(err.message);
+                } else {
+                    res.status(200).json('Course deleted.');
+                }
+            })
+        }
+    })
+}
+
+export default { create, list, read, update, remove };
